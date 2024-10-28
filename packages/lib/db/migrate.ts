7f8dc61b629809d435bd8d1521b9319dde597c9a
Dmitry Mikhaylov <business.mikhaylov@gmail.com>
2024-10-29 02:20:23 +0900

    Add defaut abilities to db initialization

--- packages/lib/db/migrate.ts
+++ packages/lib/db/migrate.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate as m } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
+import * as schema from "./schema/index";
 import "dotenv/config";
 
 const client = postgres(process.env.DATABASE_URL! as string, { max: 1 });
@@ -12,6 +13,13 @@ export const migrate = async () => {
   await m(drizzle(client), {
     migrationsFolder: migrations,
   });
+
+  const db = drizzle(client, {
+    schema,
+    logger: true,
+  })
+  await db.insert(schema.Ability).values({ keyword: "draw" });
+  await db.insert(schema.Ability).values({ keyword: "pick" });
 
   await client.end();
 };
